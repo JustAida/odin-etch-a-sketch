4fa10d9888cb0cead6f40e0e95f9fdc54766d2c3
JustAida <suradet40@gmail.com>
2022-02-10 01:51:30 +0700

    Create a function to random the background color

--- script.js
+++ script.js
@@ -9,23 +9,71 @@ function createGrids(num) {
     container.appendChild(div);
   }
 }
+
 // Create 16 * 16 grids.
 createGrids(16)
 
-// Function to change the color of the grid.
-function changeGridColor() {
-  this.style.backgroundColor = 'black'
+function randomNum() {
+  return Math.floor(Math.random() * 256);
 }
 
-const grids = document.querySelectorAll('.grid');
-grids.forEach(grid => {
-  grid.addEventListener('mouseenter', changeGridColor);
-});
+// Function to get the number of color.
+function getGridColorArray() {
+  return [randomNum(), randomNum(), randomNum()];
+}
+
+// Function to random the background color.
+function randomBackgroundColor() {
+  if (colorChangeTimes === 0) {
+    rgb = getGridColorArray();
+    this.style.backgroundColor = `rgb(${rgb[0]}, ${rgb[1]}, ${rgb[2]})`;
+    colorChangeTimes++;
+  } else if (colorChangeTimes === 9) {
+    this.style.backgroundColor = 'rgb(0, 0, 0)';
+    colorChangeTimes = 0;
+  } else {
+    // Change each number in rgb array.
+    rgb.forEach((color, index) => rgb[index] = Math.floor(color * 0.9));
+    this.style.backgroundColor = `rgb(${rgb[0]}, ${rgb[1]}, ${rgb[2]})`;
+    colorChangeTimes++;
+  }
+}
+
+// Function to detect when mouse is hovering over the grid.
+let colorChangeTimes = 0;
+let rbg;
+function detectMouseHovering() {
+  const grids = document.querySelectorAll('.grid');
+
+  // Add event listener to each grid.
+  grids.forEach(grid => {
+    grid.addEventListener('mouseenter', randomBackgroundColor);
+  });
+}
+
+detectMouseHovering()
 
 // Clear the grid when the "Reset" button is clicked.
-const reset = document.querySelector('#reset');
-reset.addEventListener('click', () => {
+const resetButton = document.querySelector('#reset');
+resetButton.addEventListener('click', () => {
+  const grids = document.querySelectorAll('.grid');
   grids.forEach(grid => {
     grid.style.backgroundColor = 'white';
   });
+});
+
+// Create a new sketch pad when the "New" button is clicked.
+const newButton = document.querySelector('#new');
+newButton.addEventListener('click', () => {
+  // Remove all the grids in the container.
+  container.textContent = "";
+
+  // Ask the user for the number squares per side.
+  numSquares = prompt('Enter the number of squares per side [Max is 100].', '16');
+  while (!(numSquares > 0 && numSquares <= 100)) {
+    alert('Please enter a number between 1-100.');
+    numSquares = prompt('Enter the number of squares per side [Max is 100].', '16');
+  }
+  createGrids(numSquares);
+  detectMouseHovering();
 });
